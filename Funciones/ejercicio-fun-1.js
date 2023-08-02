3ef9d81311fc5eb16f6d91d65a0f40d12f01cf90
benjaMatteo96 <benjaminmatteo.96@gmail.com>
2023-08-02 09:13:19 -0400

    add-bucles-file

--- Funciones/ejercicio-fun-1.js
+++ Funciones/ejercicio-fun-1.js
@@ -3,18 +3,18 @@ represente un nombre, y muestre en la consola un saludo personalizado utilizando
 nombre proporcionado. Llama a la función con tu propio nombre para verificar que 
 funcione correctamente. */
 
-function saludar (nombre){
+/* function saludar (nombre){
   console.log(`Hola ${nombre}`)
 }
-saludar ("Benjamin")
+saludar ("Benjamin") */
 
 /* 2) Crea una función llamada "multiplicar" que reciba dos parámetros numéricos y 
 devuelva el resultado de la multiplicación de ambos. Llama a la función con dos valores 2 diferentes y muestra el resultado en la consola. */
 
-function multiplicar (num1,num2) {
+/* function multiplicar (num1,num2) {
   console.log(num1 * num2 )
 }
-multiplicar(2,5)
+multiplicar(2,5) */
 
 /* 3) Crea una función llamada "areaTriangulo" que reciba dos parámetros numéricos que 
 representen la base y la altura de un triángulo, y devuelva el área del mismo. Luego, 
@@ -24,7 +24,7 @@ estas funciones para calcular el área y el perímetro de un triángulo con base
 3 y lados de 4, 5 y 6 respectivamente.
  */
 
-function areaTriangulo (num1,num2){
+/* function areaTriangulo (num1,num2){
   let area_triangulo = (num1 * num2)/2;
   console.log(`El area del triangulo es ${area_triangulo}`)
 }
@@ -37,7 +37,7 @@ function perimetroTriangulo (num1,num2,num3){
 }
 
 areaTriangulo(5,3)
-perimetroTriangulo(4,5,6)
+perimetroTriangulo(4,5,6) */
 
 /* 4) Crea una función llamada "calcularPrecio" que reciba dos parámetros numéricos que 
 representen el precio y la cantidad de un producto, y devuelva el precio total de la 
@@ -46,7 +46,7 @@ compra. Si la cantidad de productos es mayor o igual a 10, aplica un descuento d
 descuento del 20% al precio total. Llama a la función con diferentes valores de precio y 
 cantidad para verificar que funcione correctamente */
 
-function calcularPrecio (precio,cproducto) {
+/* function calcularPrecio (precio,cproducto) {
   let precio_total = precio * cproducto;
 
   if (cproducto >= 10 && cproducto < 20) {
@@ -59,7 +59,7 @@ function calcularPrecio (precio,cproducto) {
   console.log(precio_total)
 } 
 
-calcularPrecio(100,20)
+calcularPrecio(100,20) */
 
 /* 5) Crea una función llamada "esMayorDeEdad" que reciba un parámetro numérico que 
 represente la edad de una persona, y devuelva un mensaje que indique si la persona 
@@ -68,14 +68,14 @@ mayor de edad". Si la edad es menor a 18, el mensaje debe decir "Eres menor de e
 Utiliza el operador ternario para implementar esta función. Llama a la función con 
 diferentes valores de edad para verificar que funcione correctamente */
 
-function esMayorDeEdad (edadPersona){
+/* function esMayorDeEdad (edadPersona){
   if (edadPersona >= 18) {
     console.log("Eres mayor de edad");
   } else if (edadPersona < 18 ) {
     console.log("Eres menor de edad")
   }
 }
-esMayorDeEdad(19)
+esMayorDeEdad(19) */
 
 /* 6) Crea una función llamada "calcularImpuesto" que tome un parámetro numérico que 
 represente el ingreso anual de una persona, y devuelva el impuesto que debe pagar. Si 
@@ -84,7 +84,8 @@ es mayor a $10,000 pero menor o igual a $20,000, el impuesto es del 15% del ingr
 Si el ingreso es mayor a $20,000, el impuesto es del 20% del ingreso. Utiliza 
 condicionales anidados para implementar esta función. Llama a la función con 
 diferentes valores de ingreso para verificar que funcione correctamente. */
-function calcularImpuesto (ingresoAnual){
+
+/* function calcularImpuesto (ingresoAnual){
   let impuesto = 0;
   if (ingresoAnual <= 10000){
     impuesto= ingresoAnual * 0.1
@@ -97,7 +98,7 @@ function calcularImpuesto (ingresoAnual){
   }
   console.log(`Por ganar ${ingresoAnual} debes pagar un impuesto de ${impuesto}`)
 }
-calcularImpuesto(15000)
+calcularImpuesto(15000) */
 
 /* 7) Crea una función llamada "verificarDia" que reciba un parámetro numérico que 
 represente el número del día de la semana, y devuelva un mensaje que indique si es un 
@@ -107,7 +108,7 @@ control switch anidada para implementar esta función. Llama a la función con
 diferentes valores de día para verificar que funcione correctamente. 
  */
 
-function verificarDia (diaSemana){
+/* function verificarDia (diaSemana){
   switch (diaSemana){
     case 1: 
       console.log("Es un dia laboral");
@@ -135,4 +136,4 @@ function verificarDia (diaSemana){
   }
 
   verificarDia(2)
-  
+   */
